9574557a30ec3a2f645ab576844d3790ae45326b
Millian Poquet <millian.poquet@inria.fr>
2018-11-08 04:19:55 +0100

    [test] check netorcai::Error what()

--- test/error.cpp
+++ test/error.cpp
@@ -20,6 +20,20 @@ TEST(error, assert)
     #else
         EXPECT_NO_THROW(assert_wrapper(true));
         EXPECT_THROW(assert_wrapper(false), netorcai::Error);
+        try
+        {
+            assert_wrapper(false);
+            FAIL();
+        }
+        catch (const netorcai::Error & e)
+        {
+            std::string exception_msg = e.what();
+            EXPECT_NE(exception_msg.find("assert failed"), std::string::npos);
+        }
+        catch (...)
+        {
+            FAIL();
+        }
     #endif
 }
 
@@ -27,4 +41,18 @@ TEST(error, enforce)
 {
     EXPECT_NO_THROW(enforce_wrapper(true));
     EXPECT_THROW(enforce_wrapper(false), netorcai::Error);
+    try
+    {
+        enforce_wrapper(false);
+        FAIL();
+    }
+    catch (const netorcai::Error & e)
+    {
+        std::string exception_msg = e.what();
+        EXPECT_NE(exception_msg.find("assert failed"), std::string::npos);
+    }
+    catch (...)
+    {
+        FAIL();
+    }
 }
