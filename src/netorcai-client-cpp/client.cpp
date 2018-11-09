9ad63f4067edd8241e3e0f73b05954c837552c29
Millian Poquet <millian.poquet@inria.fr>
2018-11-09 01:09:07 +0100

    [code] network functions. adds SFML dependency.
    Rationale.
    - Keep it simple.
    - Keep it as multi-platform as possible.
    
    There are no sockets in the C++ standard library so...
    
    This commit also needs basic C++20 support for std::endian...

--- src/netorcai-client-cpp/client.cpp
+++ src/netorcai-client-cpp/client.cpp
@@ -1,1 +1,90 @@
 #include <netorcai-client-cpp/client.hpp>
+#include <netorcai-client-cpp/error.hpp>
+
+#include <type_traits>
+#include <utility>
+
+#include <SFML/Network/IpAddress.hpp>
+#include <SFML/Network/Socket.hpp>
+
+namespace netorcai
+{
+
+Client::~Client()
+{
+
+}
+
+void Client::connect(const std::string & hostname, unsigned short port)
+{
+    auto status = _socket.connect(sf::IpAddress(hostname), port, sf::milliseconds(500));
+    NETORCAI_ENFORCE(status == sf::Socket::Done, "Cannot connect to %s:%hu", hostname.c_str(), port);
+}
+
+void Client::close()
+{
+    _socket.disconnect();
+}
+
+std::string Client::recvString()
+{
+    // Read content size
+    uint8_t contentSizeBuf[2];
+    std::size_t receivedSize;
+    auto status = _socket.receive((void*)contentSizeBuf, 2, receivedSize);
+    NETORCAI_ENFORCE(status == sf::Socket::Done, "Cannot read content size.");
+    NETORCAI_ENFORCE(receivedSize == 2, "Cannot read content size.");
+
+    // Convert from little endian to native endian if needed
+    if constexpr (std::endian::native == std::endian::big)
+    {
+        std::swap(contentSizeBuf[0], contentSizeBuf[1]);
+    }
+
+    const uint16_t contentSize = *((uint16_t*)contentSizeBuf);
+
+    // Read content
+    std::string content;
+    content.resize(contentSize);
+    status = _socket.receive((void*)content.data(), contentSize, receivedSize);
+    NETORCAI_ENFORCE(status == sf::Socket::Done, "Cannot read content.");
+    NETORCAI_ENFORCE(receivedSize == contentSize, "Cannot read content.");
+
+    return content;
+}
+
+json Client::recvJson()
+{
+    return json::parse(recvString());
+}
+
+void Client::sendString(const std::string & message)
+{
+    NETORCAI_ENFORCE(message.size() < 65536, "message is too big (%zu bytes)", message.size());
+
+    uint16_t contentSize = message.size();
+    uint8_t contentSizeBuf[2];
+    *((uint16_t*)contentSizeBuf) = contentSize;
+
+    // Convert from native endian to little endian if needed
+    if constexpr (std::endian::native == std::endian::big)
+    {
+        std::swap(contentSizeBuf[0], contentSizeBuf[1]);
+    }
+
+    size_t sentSize;
+    auto status = _socket.send((void*)contentSizeBuf, 2, sentSize);
+    NETORCAI_ENFORCE(status == sf::Socket::Done, "Cannot send content size.");
+    NETORCAI_ENFORCE(sentSize == 2, "Cannot send content size.");
+
+    status = _socket.send((void*)message.data(), contentSize, sentSize);
+    NETORCAI_ENFORCE(status == sf::Socket::Done, "Cannot send content.");
+    NETORCAI_ENFORCE(sentSize == contentSize, "Cannot send content.");
+}
+
+void Client::sendJson(const json & message)
+{
+    sendString(message.dump());
+}
+
+} // end of netorcai namespace
--- src/netorcai-client-cpp/client.hpp
+++ src/netorcai-client-cpp/client.hpp
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <string>
+
+#include <SFML/Network/TcpSocket.hpp>
+
+#include <netorcai-client-cpp/json.hpp>
+
+namespace netorcai
+{
+
+class Client
+{
+private:
+    sf::TcpSocket _socket;
+
+public:
+    explicit Client() = default;
+    ~Client();
+
+    void connect(const std::string & hostname = "localhost", unsigned short port = 4242);
+    void close();
+
+    std::string recvString();
+    json recvJson();
+
+    void sendString(const std::string & message);
+    void sendJson(const json & message);
+};
+
+} // end of netorcai namespace
